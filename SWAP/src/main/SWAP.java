699c157d515d654ffa508bf6f14a8e924ff0f4eb
MDingas <pauloedgar2@gmail.com>
2017-12-19 15:10:07 +0000

    Criados métodos de use cases de autenticação e registo. Chamados métodos da classe UtilizadoresDAO que necessitam de ser feitos

--- SWAP/src/main/SWAP.java
+++ SWAP/src/main/SWAP.java
@@ -5,17 +5,48 @@
  */
 package main;
 
+import swap.data.UtilizadorDAO;
+
 /**
  *
  * @author MarcoSilva
  */
 public class SWAP {
-
+    
+    UtilizadorDAO utentes;
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+    }
+    
+    public String extraiChave (String email) {
+        StringBuilder res = new StringBuilder();
+        if (!email.contains("@")) {
+            return null;
+        } else {
+            for (int i = 0; i < email.length() && email.charAt(i) != '@'; i++) {
+                res.append(email.charAt(i));
+                }
+        return res.toString();
+        }
+    }
+    
+    public boolean registo (String email, String password) {
+        String chave = extraiChave(email);
+        
+        if (chave == null || !utentes.chaveExiste(chave)) {
+            return false;
+        } else {
+            utentes.addUtente(chave,password,email);
+            return true;
+        }
+    }
+    
+    public boolean autentica (String email, String password) {
+        return utentes.existePar(email,password);
     }
     
 }
--- SWAP/src/swap/data/UtilizadorDAO.java
+++ SWAP/src/swap/data/UtilizadorDAO.java
@@ -14,66 +14,16 @@ import swap.business.Utilizador;
  *
  * @author MarcoSilva
  */
-public class UtilizadorDAO implements Map<String, Utilizador> {
+public class UtilizadorDAO {
 
-    @Override
-    public int size() {
+    public boolean chaveExiste(String chave) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    public boolean existePar(String email, String password) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    @Override
-    public boolean isEmpty() {
+    public void addUtente(String chave, String password, String email) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-
-    @Override
-    public boolean containsKey(Object key) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public boolean containsValue(Object value) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public Utilizador get(Object key) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public Utilizador put(String key, Utilizador value) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public Utilizador remove(Object key) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public void putAll(Map<? extends String, ? extends Utilizador> m) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public void clear() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public Set<String> keySet() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public Collection<Utilizador> values() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public Set<Entry<String, Utilizador>> entrySet() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-    
 }
